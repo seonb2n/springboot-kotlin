5eed2de4e81373191c4a79d78ecb2bcdd4bf0acc
seonb2n <626ksb@gmail.com>
2022-04-21 22:31:21 +0900

    security 제거 후 테스트 완료 // commonResponse 반환 에러

--- userServer/src/main/kotlin/com/example/springkotlin/infrastructure/user/UserReaderImpl.kt
+++ userServer/src/main/kotlin/com/example/springkotlin/infrastructure/user/UserReaderImpl.kt
@@ -3,8 +3,8 @@ package com.example.springkotlin.infrastructure.user
 import com.example.springkotlin.domain.user.User
 import com.example.springkotlin.domain.user.service.UserReader
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Component
+import java.lang.RuntimeException
 
 @Component
 class UserReaderImpl : UserReader{
@@ -14,10 +14,10 @@ class UserReaderImpl : UserReader{
 
     override fun getUserWithUserId(userId: Long): User {
         //todo 에러 처리 로직 필요
-        return userRepository.getUserByUserId(userId) ?: throw UsernameNotFoundException("$userId can not found")
+        return userRepository.getUserByUserId(userId) ?: throw RuntimeException("$userId can not found")
     }
 
     override fun getUserWithUserNickName(userNickName: String): User {
-        return userRepository.getUserByNickName(userNickName) ?: throw UsernameNotFoundException("$userNickName can not found")
+        return userRepository.getUserByNickName(userNickName) ?: throw RuntimeException("$userNickName can not found")
     }
 }
